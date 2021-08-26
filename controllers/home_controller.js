fecf2ee9112aca10cb93fd87cc0fd50c229d2c1f
Irfan-vit <shaikmd.irfannawaz2019@vitstudent.ac.in>
2021-08-27 04:45:17 +0530

    added controller and router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,3 @@
+module.exports.home = function(req,res){
+    return res.end('<h1>Hello WOrld</h1>')
+}
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+//use express routers
+app.use('/',require('./routes'));
+
 app.listen(port,function(err) {
     if(err){
         console.log(`Error in running the server : ${err}`);
