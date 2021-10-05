e70bb4c3a5803dd667dcc11e5a0b9e9caaa3b829
Irfan-vit <shaikmd.irfannawaz2019@vitstudent.ac.in>
2021-10-06 01:23:10 +0530

    added comments delete functionality for a post.

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,5 +1,6 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
+const { post } = require('../routes');
 
 module.exports.create = function(req, res){
     Post.findById(req.body.post,function(err, post){
@@ -19,4 +20,20 @@ module.exports.create = function(req, res){
             });
         }
     })
+}
+
+module.exports.destroy = function(req, res){
+    Comment.findById(req.params.id,function(err,comment){
+        if(comment.user == req.user.id){
+            let postId = comment.post;
+
+            comment.remove();
+
+            Post.findByIdAndUpdate(postId, {$pull: {comments: req.params.id}}, function(err,post){
+                return res.redirect('back');
+            });
+        } else {
+            return res.redirect('back');
+        }
+    });
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = function(req, res){
     Post.create({
@@ -7,5 +8,21 @@ module.exports.create = function(req, res){
     },function(err,post){
         if(err){console.log("error in creating post"); return;}
         return res.redirect('back');
+    });
+}
+
+module.exports.destroy = function(req, res){
+    Post.findById(req.params.id,function(err,post){
+        //.id means converting object id into string
+        // console.log("hello");
+        if(post.user == req.user.id){
+            post.remove();
+
+            Comment.deleteMany({post: req.params.id},function(err){
+                return res.redirect('back');
+            });
+        } else {
+            return res.redirect('back');
+        }
     });
 }
