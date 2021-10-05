a2558c1544bd59d26fef796ce722703ac89c8717
Irfan-vit <shaikmd.irfannawaz2019@vitstudent.ac.in>
2021-10-05 16:00:50 +0530

    showing comments with usernames

--- models/post.js
+++ models/post.js
@@ -14,7 +14,7 @@ const postSchema = new mongoose.Schema({
     comments: [
         {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'comment'
+            ref: 'Comment'
         }
     ]
 },{
